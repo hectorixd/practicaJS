f39461c1a216bf5d89351d1bbf9b8d3b254c0f4b
hetrival <hmtv16@gmail.com>
2022-01-15 12:21:23 -0600

    Se crea la función para calcular el descuento con base en los datos proporcionados por el usuario

--- descuentos.js
+++ descuentos.js
@@ -7,6 +7,18 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;    
 }
 
+function buttonPriceDiscount() {
+    // Se guardan los valores del usuario en variables
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;    
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;    
+    // Se utiliza la función para hacer el calculo
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);  
+    // Mostramos el resultado en el navegador
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = `El precio con descuento es de: $${precioConDescuento}`;
+}
 
 // console.log(`El precio original es de $${precioOriginal}, menos un descuento del ${descuento}%, tendríamos un precio final de $${precioConDescuento}`);
 // console.log({
