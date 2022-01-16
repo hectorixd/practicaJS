08c298414fbfbc7d32dbfef55999ddd5af387359
hetrival <hmtv16@gmail.com>
2022-01-16 11:39:11 -0600

    Se agrega un objeto con cupones y se cambia la lógica para recibir un texto y aplicar un descuento

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,19 @@
 // const precioOriginal = 120;
 // const descuento = 15;
+const coupons = [
+    {
+        name: "general",
+        discount: 15
+    },
+    {
+        name: "cuate",
+        discount: 25
+    },
+    {
+        name: "vip",
+        discount: 35
+    }
+];
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -11,13 +25,24 @@ function buttonPriceDiscount() {
     // Se guardan los valores del usuario en variables
     const inputPrice = document.getElementById("inputPrice");
     const priceValue = inputPrice.value;    
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discountValue = inputDiscount.value;    
-    // Se utiliza la función para hacer el calculo
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);  
-    // Mostramos el resultado en el navegador
-    const resultPrice = document.getElementById("resultPrice");
-    resultPrice.innerText = `El precio con descuento es de: $${precioConDescuento}`;
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;    
+    
+    const isCouponValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    const userCoupon = coupons.find(isCouponValid);
+
+    if (!userCoupon) {
+        alert(`El cupón ${couponValue} no es válido`);
+    } else {
+        const descuento = userCoupon.discount;
+        const precioDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        // Mostramos el resultado en el navegador
+        const resultPrice = document.getElementById("resultPrice");
+        resultPrice.innerText = `El precio con descuento es de: $${precioDescuento}`;
+    }
 }
 
 // console.log(`El precio original es de $${precioOriginal}, menos un descuento del ${descuento}%, tendríamos un precio final de $${precioConDescuento}`);
@@ -27,3 +52,5 @@ function buttonPriceDiscount() {
 //     porcentajePrecioConDescuento,
 //     precioConDescuento
 // });
+
+
